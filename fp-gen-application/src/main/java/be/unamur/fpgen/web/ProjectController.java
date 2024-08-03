956809b2d9138bc777a426405080b7ad160bd7bf
denisschloune <denisschloune@hotmail.com>
2024-08-03 16:07:01 +0200

    repo

---
+++ fp-gen-application/src/main/java/be/unamur/fpgen/web/ProjectController.java
@@ -0,0 +1,51 @@
+package be.unamur.fpgen.web;
+
+import be.unamur.api.ProjectApi;
+import be.unamur.fpgen.mapper.domainToWeb.ProjectDomainToWebMapper;
+import be.unamur.fpgen.mapper.webToDomain.ProjectWebToDomainMapper;
+import be.unamur.fpgen.service.ProjectService;
+import be.unamur.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class ProjectController implements ProjectApi {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Override
+    public ResponseEntity<Project> createProject(@Valid ProjectCreation projectCreation) {
+        return new ResponseEntity<>(ProjectDomainToWebMapper.map(
+                projectService.createProject(ProjectWebToDomainMapper.map(projectCreation), projectCreation.getAuthorId())
+        ), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteProject(UUID projectId) {
+        return ProjectApi.super.deleteProject(projectId);
+    }
+
+    @Override
+    public ResponseEntity<Project> getProjectById(UUID projectId) {
+        return ProjectApi.super.getProjectById(projectId);
+    }
+
+    @Override
+    public ResponseEntity<ProjectsPage> searchProjectsPaginate(@NotNull @Valid ProjectType projectType, @Valid PagedProjectQuery pagedProjectQuery) {
+        return ProjectApi.super.searchProjectsPaginate(projectType, pagedProjectQuery);
+    }
+
+    @Override
+    public ResponseEntity<Project> updateProjectData(UUID projectId, @Valid List<UUID> UUID) {
+        return ProjectApi.super.updateProjectData(projectId, UUID);
+    }
+}
