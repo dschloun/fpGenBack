0a615a906817ac6829500add59bf14a9339aa07c
denisschloune <denisschloune@hotmail.com>
2024-08-23 11:48:21 +0200

    result wip

--- fp-gen-application/src/main/java/be/unamur/fpgen/web/ResultController.java
+++ fp-gen-application/src/main/java/be/unamur/fpgen/web/ResultController.java
@@ -1,10 +1,14 @@
 package be.unamur.fpgen.web;
 
 import be.unamur.api.ResultApi;
+import be.unamur.fpgen.mapper.domainToWeb.ResultDomainToWebMapper;
+import be.unamur.fpgen.mapper.webToDomain.ResultWebToDomainMapper;
 import be.unamur.fpgen.service.ResultService;
+import be.unamur.fpgen.utils.MapperUtil;
 import be.unamur.model.Result;
 import be.unamur.model.ResultCreation;
 import be.unamur.model.ResultUpdate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.validation.Valid;
@@ -21,26 +25,29 @@ public class ResultController implements ResultApi {
 
     @Override
     public ResponseEntity<Result> addResultOnDataset(UUID datasetId, @Valid ResultCreation resultCreation) {
-        return new ResponseEntity<>()
+        return new ResponseEntity<>(ResultDomainToWebMapper.map(
+                resultService.saveResult(datasetId, ResultWebToDomainMapper.map(resultCreation))), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> deleteResultById(UUID resultId) {
-        return ResultApi.super.deleteResultById(resultId);
+        resultService.deleteResult(resultId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @Override
     public ResponseEntity<Result> getResultById(UUID resultId) {
-        return ResultApi.super.getResultById(resultId);
+        return new ResponseEntity<>(ResultDomainToWebMapper.map(resultService.getResultById(resultId)), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<List<Result>> getResultListDataset(UUID datasetId) {
-        return ResultApi.super.getResultListDataset(datasetId);
+        return new ResponseEntity<>(MapperUtil.mapList(resultService.findAllResultByDatasetId(datasetId), ResultDomainToWebMapper::map), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Result> updateResultOnDataset(UUID resultId, @Valid ResultUpdate resultUpdate) {
-        return ResultApi.super.updateResultOnDataset(resultId, resultUpdate);
+        return new ResponseEntity<>(ResultDomainToWebMapper.map(
+                resultService.updateResult(ResultWebToDomainMapper.map(resultUpdate))), HttpStatus.OK);
     }
 }
