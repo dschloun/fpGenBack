69f163758f4ed1ac9a25209ab567d00dd73b512d
denisschloune <denisschloune@hotmail.com>
2024-08-29 19:43:30 +0200

    correct research add count to have correct result

--- fp-gen-infrastructure/src/main/java/be/unamur/fpgen/repository/view/JpaGenerationProjectionRepositoryCRUD.java
+++ fp-gen-infrastructure/src/main/java/be/unamur/fpgen/repository/view/JpaGenerationProjectionRepositoryCRUD.java
@@ -15,27 +15,40 @@ import java.util.UUID;
 public interface JpaGenerationProjectionRepositoryCRUD extends JpaRepository <GenerationEntity, UUID> {
 
     @Query(nativeQuery = true, value =
-    "SELECT DISTINCT ON (id) id as id, " +
-            "creation_date as creationDate, " +
-            "kind as kind, " +
-            "generation_id as generationId, " +
-            "author_trigram as authorTrigram, " +
-            "details as details, " +
-            "user_prompt as userPrompt, " +
-            "topic as topic, " +
-            "type as type, " +
-            "quantity as quantity " +
-            "FROM generation_search_view " +
-            "WHERE :kind = kind " +
-            "AND (:topic is null or topic = :topic) " +
-            "AND (:type is null or type = :type) " +
-            "AND (:authorTrigram is null or author_trigram = :authorTrigram) " +
-            "AND (:userPrompt is null or lower(user_prompt) like %:userPrompt%) " +
-            "AND (:quantity is null or quantity <= :quantity) " +
-            "AND creation_date >= cast(:startDate as timestamp) " +
-            "AND creation_date <= cast(:endDate as timestamp) " +
-            "AND ((:isIn = true AND dataset_id IN :datasetIdList) OR (:isIn = false AND id NOT IN ( " +
-                "SELECT DISTINCT id FROM generation_search_view WHERE dataset_id IN :datasetIdList ))) "
+            "SELECT DISTINCT ON (id) id as id, " +
+                    "creation_date as creationDate, " +
+                    "kind as kind, " +
+                    "generation_id as generationId, " +
+                    "author_trigram as authorTrigram, " +
+                    "details as details, " +
+                    "user_prompt as userPrompt, " +
+                    "topic as topic, " +
+                    "type as type, " +
+                    "quantity as quantity " +
+                    "FROM generation_search_view " +
+                    "WHERE :kind = kind " +
+                    "AND (:topic is null or topic = :topic) " +
+                    "AND (:type is null or type = :type) " +
+                    "AND (:authorTrigram is null or author_trigram = :authorTrigram) " +
+                    "AND (:userPrompt is null or lower(user_prompt) like %:userPrompt%) " +
+                    "AND (:quantity is null or quantity <= :quantity) " +
+                    "AND creation_date >= cast(:startDate as timestamp) " +
+                    "AND creation_date <= cast(:endDate as timestamp) " +
+                    "AND ((:isIn = true AND dataset_id IN :datasetIdList) OR (:isIn = false AND id NOT IN ( " +
+                    "SELECT DISTINCT id FROM generation_search_view WHERE dataset_id IN :datasetIdList ))) ",
+            countQuery =
+                    "SELECT COUNT(DISTINCT id) " +
+                            "FROM generation_search_view " +
+                            "WHERE :kind = kind " +
+                            "AND (:topic is null or topic = :topic) " +
+                            "AND (:type is null or type = :type) " +
+                            "AND (:authorTrigram is null or author_trigram = :authorTrigram) " +
+                            "AND (:userPrompt is null or lower(user_prompt) like %:userPrompt%) " +
+                            "AND (:quantity is null or quantity <= :quantity) " +
+                            "AND creation_date >= cast(:startDate as timestamp) " +
+                            "AND creation_date <= cast(:endDate as timestamp) " +
+                            "AND ((:isIn = true AND dataset_id IN :datasetIdList) OR (:isIn = false AND id NOT IN ( " +
+                            "SELECT DISTINCT id FROM generation_search_view WHERE dataset_id IN :datasetIdList ))) "
     )
     Page<GenerationProjection> search(@Param("kind") String kind,
                                       @Param("topic") String topic,
