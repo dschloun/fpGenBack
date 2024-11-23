26cf463b1861bdba2116e7d4f377831af776edca
denisschloune <denisschloune@hotmail.com>
2024-11-23 15:11:31 +0100

    correct

--- fp-gen-application/src/main/java/be/unamur/fpgen/mapper/domainToWeb/ConversationDomainToWebMapper.java
+++ fp-gen-application/src/main/java/be/unamur/fpgen/mapper/domainToWeb/ConversationDomainToWebMapper.java
@@ -2,7 +2,10 @@ package be.unamur.fpgen.mapper.domainToWeb;
 
 import be.unamur.fpgen.utils.MapperUtil;
 import be.unamur.model.Conversation;
+import be.unamur.model.ConversationMessage;
 import be.unamur.model.ConversationSummary;
+
+import java.util.Comparator;
 
 public class ConversationDomainToWebMapper {
     public static Conversation map(be.unamur.fpgen.conversation.Conversation domain) {
@@ -13,7 +16,7 @@ public class ConversationDomainToWebMapper {
                 .messageTopic(MessageTopicDomainToWebMapper.map(domain.getTopic()))
                 .maxInteractionNumber(domain.getMaxInteractionNumber())
                 .minInteractionNumber(domain.getMinInteractionNumber())
-                .messages(MapperUtil.mapSet(domain.getConversationMessageList(), ConversationMessageDomainToWebMapper::map).stream().toList());
+                .messages(MapperUtil.mapSet(domain.getConversationMessageList(), ConversationMessageDomainToWebMapper::map).stream().sorted(Comparator.comparing(ConversationMessage::getOrderNumber)).toList());
     }
 
     public static ConversationSummary mapSummary(be.unamur.fpgen.conversation.Conversation domain) {
