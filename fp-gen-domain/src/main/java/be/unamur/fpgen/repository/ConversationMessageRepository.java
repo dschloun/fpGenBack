d45cc98f2f5cbb9fcfd472a576d0efbc1a11344d
dschloun <denis.schloune@student.unamur.be>
2025-02-02 11:57:08 +0100

    cleaning repositories

--- fp-gen-domain/src/main/java/be/unamur/fpgen/repository/ConversationMessageRepository.java
+++ fp-gen-domain/src/main/java/be/unamur/fpgen/repository/ConversationMessageRepository.java
@@ -11,11 +11,34 @@ import org.springframework.data.domain.Pageable;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+/**
+ * Repository for conversation messages.
+ */
 public interface ConversationMessageRepository {
+    /**
+     * Save a conversation message.
+     * @param conversationMessage the conversation on which to save the message
+     * @param conversationMessageList the conversation message listto save
+     * @return the saved conversation message
+     */
     List<ConversationMessage> saveConversationMessageList(Conversation conversation, List<ConversationMessage> conversationInstantMessageList);
 
+    /**
+     * delete a conversation message.
+     * @param conversationInstantMessageId
+     */
     void deleteConversationMessageById(Long conversationInstantMessageId);
 
+    /**
+     * Find conversations messages with filters
+     * @param topic
+     * @param type
+     * @param content
+     * @param startDate
+     * @param endDate
+     * @param pageable
+     * @return a paginate result
+     */
     ConversationMessagesPage findPagination(MessageTopicEnum topic, MessageTypeEnum type, String content, OffsetDateTime startDate, OffsetDateTime endDate, Pageable pageable);
 
 }
--- fp-gen-infrastructure/src/main/java/be/unamur/fpgen/repository/author/JpaAuthorRepository.java
+++ fp-gen-infrastructure/src/main/java/be/unamur/fpgen/repository/author/JpaAuthorRepository.java
@@ -16,6 +16,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * See the specifications in the {@link AuthorRepository} interface.
+ */
 @Repository
 public class JpaAuthorRepository implements AuthorRepository {
 
